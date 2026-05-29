945b8c7d45eba39a3768235506a8d564e9948fcb
john1p23 <johnn2025q@gmail.com>
2026-05-29 01:06:58 -0500

    primer proyecto

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+// Aquí se guardarán las visitas
+const visitas = [];
+
+// Detectar IP correctamente
+app.set('trust proxy', true);
+
+// Página principal
+app.get('/', (req, res) => {
+
+    const ip = req.ip;
+
+    const navegador = req.headers['user-agent'];
+
+    const fecha = new Date().toLocaleString();
+
+    // Guardar visita
+    visitas.push({
+        ip,
+        navegador,
+        fecha
+    });
+
+    console.log('Nueva visita:', {
+        ip,
+        fecha
+    });
+
+    res.send(`
+        <h1>Bienvenido</h1>
+        <p>Tu visita fue registrada.</p>
+    `);
+
+});
+
+// Panel admin
+app.get('/admin', (req, res) => {
+
+    res.json(visitas);
+
+});
+
+// Encender servidor
+app.listen(PORT, () => {
+
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+
+});
